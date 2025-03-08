8da8d0f361da92c9d45fce39156938a16fd9af90
Hari Teja Patnala <148482244+hariteja-01@users.noreply.github.com>
2025-03-08 23:06:44 +0530

    Updated C++ code with improvements.

--- page_replacement.cpp
+++ page_replacement.cpp
@@ -1,26 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Code for FIFO Page Replacement
 
-void saveResults(string algo, int pageFaults)
-{
+// Function to write results to a file
+void saveResults(string algo, int pageFaults) {
     ofstream fout("results.txt", ios::app); // Open file in append mode
     fout << algo << " " << pageFaults << endl;
     fout.close();
 }
 
-void FIFO(vector<int> pages, int frames)
-{
+// Code for FIFO Page Replacement
+void FIFO(vector<int> pages, int frames) {
     queue<int> frameQueue;
     unordered_map<int, bool> pageMap;
     int pageFaults = 0;
-    cout << "\nFIFO Page Replacement Algorithm:\n";
-    for (int page : pages)
-    {
-        if (!pageMap[page])
-        { //  if page fault occurs
-            if (frameQueue.size() >= frames)
-            { // if it happens, remove oldest page if full
+    
+    for (int page : pages) {
+        if (!pageMap[page]) { // If page fault occurs
+            if (frameQueue.size() >= frames) { // Remove oldest page if full
                 int oldest = frameQueue.front();
                 frameQueue.pop();
                 pageMap.erase(oldest);
@@ -30,28 +26,21 @@ void FIFO(vector<int> pages, int frames)
             pageFaults++;
         }
     }
-    cout << "Total Page Faults: " << pageFaults << endl;
-    saveResults("FIFO", pageFaults);
+    cout << "FIFO Page Faults: " << pageFaults << endl;
+    saveResults("FIFO", pageFaults); // Save result for visualization
 }
 
 // Code to implement LRU Page Replacement
-void LRU(vector<int> pages, int frames)
-{
+void LRU(vector<int> pages, int frames) {
     unordered_map<int, int> pageMap;
     int pageFaults = 0;
 
-    cout << "\nLRU Page Replacement Algorithm:\n";
-    for (int i = 0; i < pages.size(); i++)
-    {
-        if (pageMap.find(pages[i]) == pageMap.end())
-        { // Page fault
-            if (pageMap.size() >= frames)
-            {
+    for (int i = 0; i < pages.size(); i++) {
+        if (pageMap.find(pages[i]) == pageMap.end()) { // Page fault
+            if (pageMap.size() >= frames) {
                 int lru = INT_MAX, pageToRemove;
-                for (auto p : pageMap)
-                {
-                    if (p.second < lru)
-                    {
+                for (auto p : pageMap) {
+                    if (p.second < lru) {
                         lru = p.second;
                         pageToRemove = p.first;
                     }
@@ -62,60 +51,47 @@ void LRU(vector<int> pages, int frames)
         }
         pageMap[pages[i]] = i; // Update last used index
     }
-    cout << "Total Page Faults: " << pageFaults << endl;
+    cout << "LRU Page Faults: " << pageFaults << endl;
     saveResults("LRU", pageFaults);
 }
 
 // Code for Optimal Page Replacement
-void Optimal(vector<int> pages, int frames)
-{
+void Optimal(vector<int> pages, int frames) {
     vector<int> frame;
     int pageFaults = 0;
 
-    cout << "\nOptimal Page Replacement Algorithm:\n";
-    for (int i = 0; i < pages.size(); i++)
-    {
+    for (int i = 0; i < pages.size(); i++) {
         auto it = find(frame.begin(), frame.end(), pages[i]);
-        if (it == frame.end())
-        { // Page fault
-            if (frame.size() >= frames)
-            {
+        if (it == frame.end()) { // Page fault
+            if (frame.size() >= frames) {
                 int farthest = -1, pageToReplace;
-                for (int j = 0; j < frame.size(); j++)
-                {
+                for (int j = 0; j < frame.size(); j++) {
                     int k;
-                    for (k = i + 1; k < pages.size(); k++)
-                    {
-                        if (pages[k] == frame[j])
-                            break;
+                    for (k = i + 1; k < pages.size(); k++) {
+                        if (pages[k] == frame[j]) break;
                     }
-                    if (k == pages.size())
-                    { // Page not used again
+                    if (k == pages.size()) { // Page not used again
                         pageToReplace = j;
                         break;
                     }
-                    if (k > farthest)
-                    {
+                    if (k > farthest) {
                         farthest = k;
                         pageToReplace = j;
                     }
                 }
                 frame[pageToReplace] = pages[i];
-            }
-            else
-            {
+            } else {
                 frame.push_back(pages[i]);
             }
             pageFaults++;
         }
     }
-    cout << "Total Page Faults: " << pageFaults << endl;
+    cout << "Optimal Page Faults: " << pageFaults << endl;
     saveResults("Optimal", pageFaults);
 }
 
-// The main function of cpp to run the above functions (also called as Driver Code).
-int main()
-{
+// The main function of cpp to run the above functions.
+int main() {
     int frames, n;
     cout << "Enter the number of frames: ";
     cin >> frames;
@@ -124,11 +100,11 @@ int main()
 
     vector<int> pages(n);
     cout << "Enter the page reference sequence: ";
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> pages[i];
     }
-    // close the file, so that memory leak issues will not come.
+
+    // Clear previous results
     ofstream fout("results.txt");
     fout.close();
 
